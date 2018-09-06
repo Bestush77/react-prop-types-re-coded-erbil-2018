46280e0239e280eaac759a7cb2c133a40200cd75
Bestun Gardi <bestun95@yahoo.com>
2018-09-06 07:37:48 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // Code Goes Here
 import React from 'react';
 import PropTypes from 'prop-types';
+
+
+
  class Order extends React.Component {
- 
   render() {
     return (
       <div className="order">
